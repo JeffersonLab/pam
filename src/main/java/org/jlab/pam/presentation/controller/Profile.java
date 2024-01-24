669a06d42c7c757510168222adc96b31f2e785f8
slominskir <ryan.slominski@gmail.com>
2024-01-24 15:59:49 -0500

    profile

--- src/main/java/org/jlab/pam/presentation/controller/Profile.java
+++ src/main/java/org/jlab/pam/presentation/controller/Profile.java
@@ -3,15 +3,14 @@ package org.jlab.pam.presentation.controller;
 import org.wildfly.security.auth.server.SecurityDomain;
 import org.wildfly.security.auth.server.SecurityIdentity;
 import org.wildfly.security.authz.Attributes;
+import org.wildfly.security.http.oidc.OidcSecurityContext;
 
-import javax.security.enterprise.SecurityContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -35,6 +34,17 @@ public class Profile extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        OidcSecurityContext context = (OidcSecurityContext) request.getAttribute(OidcSecurityContext.class.getName());
+
+        if(context != null) {
+            System.out.println("AccessToken: " + context.getTokenString());
+            System.out.println("Given Name: " + context.getToken().getGivenName());
+            System.out.println("Family Name: " + context.getToken().getFamilyName());
+            System.out.println("IDToken: " + context.getIDTokenString());
+            System.out.println("Given Name: " + context.getIDToken().getGivenName());
+            System.out.println("Family Name: " + context.getIDToken().getFamilyName());
+        }
 
         SecurityDomain domain = SecurityDomain.getCurrent();
         SecurityIdentity identity = domain.getCurrentSecurityIdentity();
